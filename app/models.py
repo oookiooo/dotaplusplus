7da5810fc29f222e4d20f5ecae4bf992e7ef2ea4
Arkadiusz Kiszka <00oookiocz@gmail.com>
2024-05-28 20:22:18 +0200

    fix: poprawka błędów przy zapisie do db

--- app/models.py
+++ app/models.py
@@ -1,24 +1,26 @@
-# models.py
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from datetime import datetime
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
     full_name = Column(String, index=True)
 
-    messages = relationship("Message", back_populates="owner")
+    messages_sent = relationship("Message", foreign_keys='Message.sender_id', back_populates="sender")
+    messages_received = relationship("Message", foreign_keys='Message.recipient_id', back_populates="recipient")
     inbox = relationship("Inbox", back_populates="owner")
+
 
 class Message(Base):
     __tablename__ = "messages"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     sender_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     recipient_id = Column(Integer, ForeignKey("users.id"), nullable=False)
@@ -26,15 +28,17 @@ class Message(Base):
     body = Column(Text)
     timestamp = Column(DateTime, default=datetime.utcnow)
 
-    owner = relationship("User", back_populates="messages")
+    sender = relationship("User", foreign_keys=[sender_id], back_populates="messages_sent")
+    recipient = relationship("User", foreign_keys=[recipient_id], back_populates="messages_received")
+
 
 class Inbox(Base):
     __tablename__ = "inboxes"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     message_id = Column(Integer, ForeignKey("messages.id"), nullable=False)
-
+    is_read = Column(Boolean, default=False)
 
     owner = relationship("User", back_populates="inbox")
     message = relationship("Message")
